f7075b942dcd00899315d0047f25aa8e28abaebb
helle253 <nathanhellbhoy@gmail.com>
2023-03-13 13:21:22 -0500

    Fix incorrect find_by, change instances of 'user'

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def show
-    @current_administrator = Administrator.find_by(session[:current_administrator_id])
-    @current_user = session[:current_user_email]
+    @current_administrator = Administrator.find_by(id: session[:current_administrator_id])
+    @current_recipient = Recipient.find_by(id: session[:current_recipient_id])
   end
 end
