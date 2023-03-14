f910c890bc81099252f227ef3100abb4064ce517
helle253 <nathanhellbhoy@gmail.com>
2023-03-13 21:05:59 -0500

    remove extra homepage routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homepage#show'
-  resource :homepage, controller: 'homepage'
+  resource :homepage, controller: 'homepage', only: :show
   namespace :admin do
     resource :session, only: %i[new create]
     resource :dashboard, controller: 'dashboard', only: :show
