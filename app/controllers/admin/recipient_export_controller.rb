b534c7fbd9376f5db80b164fa5c446e25973de98
Nathan-Heller <nheller@bluebottlecoffee.com>
2023-03-17 15:34:26 -0500

    add csv export

---
+++ app/controllers/admin/recipient_export_controller.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+module Admin
+  class RecipientExportController < ApplicationController
+    def show
+      respond_to do |format|
+        format.html
+        format.csv do
+          send_data Recipient.to_csv,
+                    filename: "mailing-list-recipients-#{DateTime.now.strftime('%d%m%Y%H%M')}.csv"
+        end
+      end
+    end
+  end
+end
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,14 @@
 class Recipient < ApplicationRecord
   has_many :recipient_allergens, dependent: :destroy
   has_many :allergens, through: :recipient_allergens
+
+  def self.to_csv
+    recipients = all
+    CSV.generate do |csv|
+      csv << %w[email notes allergies].freeze
+      recipients.each do |r|
+        csv << [r.email, r.notes, r.allergens.map(&:title).join('|')]
+      end
+    end
+  end
 end
