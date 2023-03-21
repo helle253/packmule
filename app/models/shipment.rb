001886a93bfb26b77d2fda8760cae98d07b80d9d
Nathan-Heller <nheller@bluebottlecoffee.com>
2023-03-21 18:11:57 -0500

    use a default image

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,10 @@
 class Shipment < ApplicationRecord
   has_one_attached :image
 
-  validates :image, presence: true
+  scope :orderable, lambda {
+    where.not(fulfillment_date: nil).
+      where('? < fulfillment_date', Time.zone.today + 2)
+  }
 
   def in_future?
     fulfillment_date.present? && fulfillment_date > Time.zone.today
