e0e9dc0689f7e99883b80e7f0700e29bfee6f5b4
Nathan-Heller <nheller@bluebottlecoffee.com>
2023-03-21 18:41:20 -0500

    add shipment_cards for homepage (stubbed without functionality)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,5 @@
 class HomepageController < ApplicationController
   def show
-    @current_administrator = Administrator.find_by(id: session[:current_administrator_id])
-    @current_recipient = Recipient.find_by(id: session[:current_recipient_id])
+    @orderable_shipments = Shipment.orderable.sort_by(&:fulfillment_date).first(4)
   end
 end
